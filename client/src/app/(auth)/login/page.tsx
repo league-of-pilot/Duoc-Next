6eaa1eaa2788aabec8ff3896e98d2fdb73d12ec2
Danh-m1 <danh2k@nexondv.com>
2024-07-02 22:03:32 +0700

    demo page - routing and folder Next convention

---
+++ client/src/app/(auth)/login/page.tsx
@@ -0,0 +1,8 @@
+// Bắt buộc dùng export default
+// https://nextjs.org/docs/app/building-your-application/routing/defining-routes#creating-ui
+// https://nextjs.org/docs/app/building-your-application/routing/colocation
+// -> naming convention của Next
+
+export default function loginPage() {
+  return <div>loginPage</div>
+}
---
+++ client/src/app/(auth)/register/page.tsx
@@ -0,0 +1,4 @@
+// Tên page ko quan trọng
+export default function Register() {
+  return <div>Register</div>
+}
