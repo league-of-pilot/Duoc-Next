1e68c5cd2045e5e9ccbea66b42bfc77b49204c90
Danh-m1 <danh2k@nexondv.com>
2024-07-02 21:52:57 +0700

    vid9 - img trong Next

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+// https://nextjs.org/docs/pages/building-your-application/optimizing/images#remote-images
+// https://nextjs.org/docs/app/building-your-application/optimizing/images
 const nextConfig = {}
 
 export default nextConfig
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image'
+import imgImport from '../../public/images/suffer.png'
 
 export default function Home() {
   return (
@@ -6,6 +7,29 @@ export default function Home() {
       <h1 className='text-4xl text-center font-thin font-sans'>
         Hello roboto font local
       </h1>
+
+      <div className=' w-[700px] h-[700px] bg-red-300'>
+        <Image
+          src='/images/suffer.png'
+          alt='Picture of the author'
+          width={500} // import trực tiếp phải provide width - height
+          height={500}
+          quality={100} // optional
+          // className='h-[500px]' -> ko được, phải dùng props
+        />
+      </div>
+
+      <hr />
+
+      <div className=' w-[700px] h-[700px] bg-red-300'>
+        {/* import thông qua import ko cần */}
+        <Image src={imgImport} alt='Picture of the author' />
+      </div>
+
+      {/* Dùng thẻ img gốc sẽ bị eslint Next warning */}
+      <hr />
+
+      {/* <img src='/suffer.png' alt='Picture of the author' /> */}
     </main>
   )
 }
