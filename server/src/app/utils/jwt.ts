2db52f7fb4e0ac6e3dc447898d609f0a01f82605
Danh Tran <danh2k@nexondv.com>
2024-07-09 10:08:16 +0700

    small rename

--- server/src/app/utils/jwt.ts
+++ server/src/app/utils/jwt.ts
@@ -1,7 +1,7 @@
 import { envConfig } from '@app/const/config.const'
 import { SESSION_TOKEN_EXPIRES_IN } from '@app/const/global.const'
 import { TokenType } from '@app/const/token.const'
-import { TokenPayload } from '@app/types/jwt.types'
+import { TokenPayload } from '@app/types/jwt.type'
 import { SignerOptions, createSigner, createVerifier } from 'fast-jwt'
 
 export const signSessionToken = (
