6d0ab73385ea7ae68d60af5e1acf5bcbaf4b3888
Danh-m1 <danh2k@nexondv.com>
2024-07-12 00:36:10 +0700

    http interceptor set new expired session token

--- client/src/nextApp/apiRequest/sessionToken.ts
+++ client/src/nextApp/apiRequest/sessionToken.ts
@@ -2,6 +2,8 @@ import { isClient } from '../nextApp.utils'
 
 class SessionToken {
   private token = ''
+  private _expiresAt = new Date().toISOString()
+
   get value() {
     return this.token
   }
@@ -12,6 +14,17 @@ class SessionToken {
     }
     console.log('🚀 sessionToken set', token.slice(-5))
     this.token = token
+  }
+
+  get expiresAt() {
+    return this._expiresAt
+  }
+  set expiresAt(expiresAt: string) {
+    // Nếu gọi method này ở server thì sẽ bị lỗi
+    if (typeof window === 'undefined') {
+      throw new Error('Cannot set token on server side')
+    }
+    this._expiresAt = expiresAt
   }
 }
 
