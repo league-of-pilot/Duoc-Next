c4256c01f00b1ad681dd189f1c39ba72e931300f
Danh-m1 <danh2k@nexondv.com>
2024-07-12 08:26:45 +0700

    FEAT: handle upload - delete - submit form

--- client/src/module/productsAddForm/ProductsAddForm.tsx
+++ client/src/module/productsAddForm/ProductsAddForm.tsx
@@ -13,27 +13,54 @@ import { Input } from '@/components/ui/input'
 import { useToast } from '@/components/ui/use-toast'
 import { handleErrorApi } from '@/nextApp/apiRequest/fetch.utils'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
 import { useForm } from 'react-hook-form'
 import {
   CreateProductBodyType,
   createProductSchema
 } from './ProductsAddForm.schema'
 import { Textarea } from '@/components/ui/textarea'
+import Image from 'next/image'
+import productApiRequest from '@/nextApp/apiRequest/product/product.api'
 
 const ProductAddForm = () => {
+  const [file, setFile] = useState<File | null>(null)
+  const inputRef = useRef<HTMLInputElement | null>(null)
+
   const [loading, setLoading] = useState(false)
   const { toast } = useToast()
   const router = useRouter()
   const form = useForm<CreateProductBodyType>(createProductSchema)
 
+  const deleteFile = () => {
+    setFile(null)
+    form.setValue('image', '')
+    if (inputRef.current) {
+      inputRef.current.value = ''
+    }
+  }
+
   async function onSubmit(values: CreateProductBodyType) {
     if (loading) return
     setLoading(true)
     try {
-      // TODO try submit here
+      const formData = new FormData()
+      formData.append('file', file as Blob)
+
+      // Logic BE đang xử lý riêng, up file trước, sau đó trả về url,
+      // FE lấy url này gọi tạo file -> ko hay
+      // 2 step tạo form rất dở
+
+      const uploadImageResult = await productApiRequest.uploadImage(formData)
+      const imageUrl = uploadImageResult.payload.data
+
+      const result = await productApiRequest.create({
+        ...values,
+        image: imageUrl
+      })
+
       toast({
-        description: 'payload.message'
+        description: result.payload.message
       })
       router.push('/products')
     } catch (error: any) {
@@ -102,12 +129,53 @@ const ProductAddForm = () => {
             <FormItem>
               <FormLabel>Hình ảnh</FormLabel>
               {/* TODO Add Form Hình ảnh input upload */}
+              <FormControl>
+                <Input
+                  type='file'
+                  accept='image/*'
+                  // ref={field.ref}
+                  // https://react-hook-form.com/docs/usecontroller/controller
+                  // https://www.react-hook-form.com/faqs/#Howtosharerefusage
+                  ref={inputRef}
+                  onChange={e => {
+                    const file = e.target.files?.[0]
+                    if (file) {
+                      // set file để render ra
+                      setFile(file)
+                      // set vào form url img, chỗ này xử lý ko hay
+                      // real sẽ phải làm khác
+                      // 1 phần do upload img là api riêng, sau đó chỉ hứng url về
+                      // 1 phần do validate thống nhất với data, url này chỉ mang ý nghãi dummy pass qua zod
+                      field.onChange('http://localhost:3000/' + file.name)
+                    }
+                  }}
+                />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
         />
 
-        {/* TODO Display hình ảnh đang chọn */}
+        {file && (
+          <div>
+            <Image
+              src={URL.createObjectURL(file)}
+              width={128}
+              height={128}
+              alt='preview'
+              className='w-32 h-32 object-cover'
+            />
+            <Button
+              type='button'
+              variant={'destructive'}
+              size={'sm'}
+              // Vì chỉ upload 1 file duy nhất nên hàm gọi rỗng ko cần tham số
+              onClick={deleteFile}
+            >
+              Xóa hình ảnh
+            </Button>
+          </div>
+        )}
 
         <Button type='submit' className='!mt-8 w-full'>
           Thên sản phẩm
